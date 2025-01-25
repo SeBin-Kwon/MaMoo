5f4441869e102d7028a78fe00df317bef385b5dd
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-26 01:24:01 +0900

    Feat: UserDefaultsManger 추가

---
+++ MaMoo/Manager/UserDefaultsManager.swift
@@ -0,0 +1,57 @@
+//
+//  UserDefaultsManager.swift
+//  MaMoo
+//
+//  Created by Sebin Kwon on 1/26/25.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    private init() {}
+    private let userDefaults = UserDefaults.standard
+    
+    enum UserDefaultsKey: String {
+        case isDisplayedOnboarding
+        case nickname
+        case profileImage
+        case like
+    }
+    
+    func getter<T>(key: UserDefaultsKey, defaultValue: T) -> T {
+        userDefaults.object(forKey: key.rawValue) as? T ?? defaultValue
+    }
+    
+    func setter<T>(value: T, key: UserDefaultsKey) {
+        userDefaults.set(value, forKey: key.rawValue)
+    }
+    
+    var isDisplayedOnboarding: Bool {
+        get {
+            getter(key: .isDisplayedOnboarding, defaultValue: false)
+        }
+        set {
+            setter(value: newValue, key: .isDisplayedOnboarding)
+        }
+    }
+    
+    var nickname: String {
+        get {
+            getter(key: .nickname, defaultValue: "")
+        }
+        set {
+            setter(value: newValue, key: .nickname)
+        }
+    }
+    
+    var profileImage: Int {
+        get {
+            getter(key: .profileImage, defaultValue: 0)
+        }
+        set {
+            setter(value: newValue, key: .profileImage)
+        }
+    }
+    
+}
