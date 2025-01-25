0a8330ceb7cae6dab2137ecc9eb4f959f3627899
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-26 00:37:14 +0900

    Feat: ProfileViewController UI 업데이트

--- MaMoo/Profile/ProfileViewController.swift
+++ MaMoo/Profile/ProfileViewController.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
-class ProfileViewController: UIViewController {
-
+final class ProfileViewController: BaseViewController {
+    
+    private let num = Int.random(in: 0...11)
+    
+    private lazy var profileImageButton = ProfileImageButton(num: 7)
+    
+    private let textField = {
+        let textfield = UITextField()
+        textfield.borderStyle = .none
+        textfield.backgroundColor = .white
+        return textfield
+    }()
+    
+    private let button = MaMooButton(title: "완료")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.title = "프로필 설정"
+        profileImageButton.addTarget(self, action: #selector(profileImageButtonTapped), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func profileImageButtonTapped() {
+        print(#function)
     }
-    */
+    
+    override func viewDidLayoutSubviews() {
+        profileImageButton.configureView()
+    }
+    
+    override func configureView() {
+        view.addSubview(profileImageButton)
+        view.addSubview(textField)
+        view.addSubview(button)
+        
+        profileImageButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.centerX.equalToSuperview()
+        }
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageButton.snp.bottom).offset(60)
+            make.horizontalEdges.equalToSuperview()
+        }
+        button.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(30)
+            make.horizontalEdges.equalToSuperview()
+        }
+    }
 
+}
+
+extension ProfileViewController: UITextFieldDelegate {
+    
 }
