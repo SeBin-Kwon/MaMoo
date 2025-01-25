d1c72ec0e14f7f9e4956371ccb10f5b36b9a9308
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-25 16:35:16 +0900

    Feat: 네비게이션바 커스텀

--- MaMoo/Base/BaseViewController.swift
+++ MaMoo/Base/BaseViewController.swift
@@ -14,7 +14,13 @@ class BaseViewController: UIViewController {
         configureView()
         view.backgroundColor = .black
         let backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+        backBarButtonItem.tintColor = .maMooPoint
         navigationItem.backBarButtonItem = backBarButtonItem
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .black
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
     }
     
     func configureView() {
