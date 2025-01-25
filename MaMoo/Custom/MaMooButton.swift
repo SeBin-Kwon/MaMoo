dcbcbe9143f331605f34e20d7f5778946b5af3c0
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-25 15:52:43 +0900

    Feat: MaMooButton 커스텀뷰 추가

---
+++ MaMoo/Custom/MaMooButton.swift
@@ -0,0 +1,32 @@
+//
+//  MaMooButton.swift
+//  MaMoo
+//
+//  Created by Sebin Kwon on 1/24/25.
+//
+
+import UIKit
+
+final class MaMooButton: UIButton {
+    let title: String
+    
+    init(title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        var config = UIButton.Configuration.plain()
+        var attributedTitle = AttributedString(title)
+        attributedTitle.font = .systemFont(ofSize: 16, weight: .bold)
+        config.attributedTitle = attributedTitle
+        config.baseForegroundColor = .maMooPoint
+        config.cornerStyle = .capsule
+        config.background.strokeColor = .maMooPoint
+        config.background.strokeWidth = 2
+        config.contentInsets = NSDirectionalEdgeInsets(top: 14, leading: 0, bottom: 14, trailing: 0)
+        self.configuration = config
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
