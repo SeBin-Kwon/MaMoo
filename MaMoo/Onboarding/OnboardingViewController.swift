b11e8a0129f8febd908ca380405986e4e74a60be
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-25 16:20:00 +0900

    Feat: OnboardingView 구현

--- MaMoo/Onboarding/OnboardingViewController.swift
+++ MaMoo/Onboarding/OnboardingViewController.swift
@@ -6,24 +6,66 @@
 //
 
 import UIKit
+import SnapKit
 
-class OnboardingViewController: UIViewController {
-
+final class OnboardingViewController: BaseViewController {
+    
+    private let imageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "onboarding")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    private let titleLabel = {
+        let label = UILabel()
+        label.text = "Onboarding"
+        label.textColor = .white
+        label.font = UIFont.italicSystemFont(ofSize: 37, weight: .bold)
+        
+        return label
+    }()
+    
+    private let contentLabel = {
+        let label = UILabel()
+        label.text = "당신만의 영화 세상,\nMAMOO를 시작해보세요."
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    private let button = MaMooButton(title: "시작하기")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func buttonTapped() {
+        navigationController?.pushViewController(ProfileViewController(), animated: true)
     }
-    */
-
+    
+    override func configureView() {
+        view.addSubview(imageView)
+        view.addSubview(titleLabel)
+        view.addSubview(contentLabel)
+        view.addSubview(button)
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(30)
+            make.horizontalEdges.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(imageView.snp.bottom)
+        }
+        contentLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(30)
+        }
+        button.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(contentLabel.snp.bottom).offset(30)
+        }
+    }
+    
 }
