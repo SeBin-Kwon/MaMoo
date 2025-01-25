89f7aa938a6f9a9990136746da42461ce7c7ea5c
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-26 02:00:33 +0900

    Feat: UICollectionViewCell 익스텐션 추가

---
+++ MaMoo/Extension/UICollectionViewCell+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UICollectionViewCell+Extension.swift
+//  MaMoo
+//
+//  Created by Sebin Kwon on 1/26/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
+
+
