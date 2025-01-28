2eeee79c9b6f990e17031b749bb3b1feffbd457c
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-29 02:50:05 +0900

    Feat: MainView UI 및 버튼 연결

--- MaMoo/Main/MainView.swift
+++ MaMoo/Main/MainView.swift
@@ -6,9 +6,86 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainView: BaseView {
-
+    let profileEditButton = ProfileEditButton()
+    private lazy var searchTitleLabel = configureTitleLabel("최근검색어")
+    let allRemoveButton = {
+        let btn = UIButton()
+        var config = UIButton.Configuration.plain()
+        var attributedTitle = AttributedString("전체 삭제")
+        attributedTitle.font = .systemFont(ofSize: 13, weight: .bold)
+        attributedTitle.foregroundColor = UIColor.maMooPoint
+        config.attributedTitle = attributedTitle
+        btn.configuration = config
+        return btn
+    }()
+    let searchStackView = {
+        let stack = UIStackView()
+        stack.backgroundColor = .red
+        stack.axis = .horizontal
+        stack.alignment = .center
+        stack.distribution = .fillProportionally
+        stack.spacing = 10
+        stack.isUserInteractionEnabled = true
+        return stack
+    }()
+    private let scrollView = {
+       let scroll = UIScrollView()
+        scroll.showsHorizontalScrollIndicator = false
+        scroll.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        scroll.backgroundColor = .blue
+        scroll.isUserInteractionEnabled = true
+        return scroll
+    }()
+    private lazy var todayTitleLabel = configureTitleLabel("오늘의 영화")
+//    let collectionView = UICollectionView()
     
-
+    override func configureHierarchy() {
+        addSubview(profileEditButton)
+        addSubview(searchTitleLabel)
+        addSubview(allRemoveButton)
+        scrollView.addSubview(searchStackView)
+        addSubview(scrollView)
+        addSubview(todayTitleLabel)
+//        addSubview(collectionView)
+    }
+    
+    override func configureLayout() {
+        profileEditButton.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(30)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(10)
+            make.height.equalTo(140)
+        }
+        searchTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileEditButton.snp.bottom).offset(10)
+            make.leading.equalTo(safeAreaLayoutGuide).offset(10)
+        }
+        allRemoveButton.snp.makeConstraints { make in
+            make.centerY.equalTo(searchTitleLabel)
+            make.trailing.equalTo(safeAreaLayoutGuide).inset(10)
+        }
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(searchTitleLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide)
+//            make.height.equalTo(30)
+        }
+        searchStackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(scrollView)
+            make.height.equalTo(scrollView.snp.height)
+        }
+        todayTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.bottom).offset(10)
+            make.leading.equalTo(safeAreaLayoutGuide).offset(10)
+        }
+    }
+    
+    private func configureTitleLabel(_ title: String) -> UILabel {
+        let label = UILabel()
+        label.text = title
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.textColor = .white
+        return label
+    }
 }
--- MaMoo/Main/MainViewController.swift
+++ MaMoo/Main/MainViewController.swift
@@ -10,27 +10,54 @@ import SnapKit
 import Alamofire
 
 class MainViewController: BaseViewController {
+
+    private let mainView = MainView()
     
-    private let profileEditButton = ProfileEditButton()
-//    private let mainView = MainView()
-//    override func loadView() {
-//        view = mainView
-//    }
+    override func loadView() {
+        view = mainView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "MAMOO"
-        configureView()
         let rightItem = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(rightItemTapped))
         rightItem.tintColor = .maMooPoint
         navigationItem.rightBarButtonItem = rightItem
-        
-        
-        profileEditButton.nicknameLabel.text = UserDefaultsManager.shared.nickname
-        profileEditButton.profileImage.image = UIImage(named: "profile_\(UserDefaultsManager.shared.profileImage)")
-        profileEditButton.dateLabel.text = UserDefaultsManager.shared.signUpDate
-        
-        
+        configureData()
+        callRequest()
+    }
+    
+    @objc func rightItemTapped() {
+        print(#function)
+    }
+    
+    private func configureData() {
+        mainView.profileEditButton.nicknameLabel.text = UserDefaultsManager.shared.nickname
+        mainView.profileEditButton.profileImage.image = UIImage(named: "profile_\(UserDefaultsManager.shared.profileImage)")
+        mainView.profileEditButton.dateLabel.text = UserDefaultsManager.shared.signUpDate
+        for i in 0..<10 {
+            let tag = SearchTagButton()
+            tag.searchLabel.text = "어벤져스 \(i)"
+            tag.addTarget(self, action: #selector(tagtapped), for: .touchUpInside)
+            tag.removeButton.addTarget(self, action: #selector(removeButtontapped), for: .touchUpInside)
+            mainView.searchStackView.addArrangedSubview(tag)
+        }
+        mainView.profileEditButton.addTarget(self, action: #selector(profileEditButtontapped), for: .touchUpInside)
+    }
+    
+    @objc private func profileEditButtontapped() {
+        print(#function)
+    }
+    
+    @objc private func removeButtontapped() {
+        print(#function)
+    }
+    
+    @objc private func tagtapped() {
+        print(#function)
+    }
+    
+    private func callRequest() {
         NetworkManager.shared.fetchResults(api: TMDBRequest.trending, type: Movie.self) { value in
             print(value.results.count)
         } failHandler: {
@@ -38,21 +65,14 @@ class MainViewController: BaseViewController {
         }
     }
     
-    @objc func rightItemTapped() {
-        print(#function)
-    }
-    
-    
 
     override func configureView() {
-        view.addSubview(profileEditButton)
-        
-
-        profileEditButton.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(30)
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
-            make.height.equalTo(140)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        mainView.searchStackView.subviews.forEach {
+            guard let btn = $0 as? SearchTagButton else { return }
+            btn.backgroundView.layer.cornerRadius = mainView.searchStackView.frame.height / 2
         }
-        
     }
 }
